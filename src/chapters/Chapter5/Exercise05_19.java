8aed039470f16cf4300d6b35c6673af11bf59fac
aliozclk <alliozcelik96@gmail.com>
2022-09-13 22:58:27 +0300

    new exercises added

--- src/chapters/Chapter5/Exercise05_19.java
+++ src/chapters/Chapter5/Exercise05_19.java
@@ -7,13 +7,13 @@ public class Exercise05_19 {
 		
 		for(int i = 1 ; i <= 8 ; i++) {
 			for(int j = 1 ; j <= 16-2*i ; j++ ) {
-				System.out.print(" ");
+				System.out.print("   ");
 			}
 			for(int k = 0 ; k < i ; k++ ) {
-				System.out.printf("%2d",(int)(Math.pow(2, k)), " ");
+				System.out.printf("%3d%s",(int)(Math.pow(2, k)),"   ");
 			}
 			for(int t = i-2; t >= 0 ; t -- ) {
-					System.out.printf("%2d",(int)Math.pow(2, t) , " ");
+					System.out.printf("%3d%s",(int)Math.pow(2, t),"   ");
 				
 			}
 			System.out.println();
