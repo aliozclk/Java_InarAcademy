838a892f1200d27871e0a35645805f6203161763
aliozclk <alliozcelik96@gmail.com>
2022-09-16 18:21:51 +0300

    new exercises added

---
+++ src/chapters/Chapter5/Exercise05_37.java
@@ -0,0 +1,28 @@
+package chapters.Chapter5;
+
+import java.util.Scanner;
+
+public class Exercise05_37 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer : ");
+        int keyboard = input.nextInt();
+
+        int num = keyboard ;
+        int i = 0 ;
+        int binaryNum = 0 ;
+
+        while (Math.pow(2,i) <= num){
+            i++;
+        }
+
+        for(int j = 0 ; j <= i ; j++  ){
+            binaryNum += (num%2) * Math.pow(10, j );
+            num = num/2 ;
+
+        }
+        System.out.println("binary value of " +keyboard + " is " + binaryNum );
+
+    }
+}
