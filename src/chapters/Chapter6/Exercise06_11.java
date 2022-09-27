cc83cdd775b33da6cd2b58a13e759178ef917c37
aliozclk <alliozcelik96@gmail.com>
2022-09-27 23:01:57 +0300

    new exercises added

---
+++ src/chapters/Chapter6/Exercise06_11.java
@@ -0,0 +1,24 @@
+package chapters.Chapter6;
+
+public class Exercise06_11 {
+    public static void main(String[] args) {
+
+        System.out.println("Sales Amount\t\tCommission");
+        System.out.println("______________________________");
+
+        for(double i = 10_000 ; i <= 100_000 ; i += 5000 ){
+            System.out.printf("%-12.0f\t\t%6.1f\n", i , computeCommission(i));
+        }
+    }
+
+    public static double computeCommission(double salesAmount){
+        if(salesAmount >= 10000){
+            return   (salesAmount-10000) * 0.12 + 5_000 * 0.10 + 5_000 * 0.08 ;
+        }
+        else if (salesAmount >= 5000 ){
+            return  (salesAmount - 5000) * 0.10 + 5000 * 0.08 ;
+        }else {
+            return salesAmount * 0.08 ;
+        }
+    }
+}
