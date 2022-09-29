1467072574daec997bba726ecc5f56ccaa5665e9
aliozclk <alliozcelik96@gmail.com>
2022-09-29 19:11:24 +0300

    new exercises added

---
+++ src/chapters/Chapter6/Exercise06_22.java
@@ -0,0 +1,28 @@
+package chapters.Chapter6;
+
+import java.util.Scanner;
+
+public class Exercise06_22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        long num = input.nextLong();
+
+        System.out.println("The square root of the number is " + sqrt(num));
+
+    }
+
+    public static double sqrt(long n){
+        double lastGuess = 1;
+        double nextGuess = 0;
+        while(!(Math.abs(lastGuess-nextGuess) <= 0.0001) ){
+            nextGuess = (lastGuess + (double) n / lastGuess) / 2.0 ;
+            double temp = lastGuess;
+            lastGuess = nextGuess;
+            nextGuess = temp;
+        }
+
+        return nextGuess;
+    }
+}
