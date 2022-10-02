f0685f6721bf0a6471c6ecea80621b1bf91f2c10
aliozclk <alliozcelik96@gmail.com>
2022-10-02 16:23:46 +0300

    new exercises added

--- src/chapters/Chapter6/Exercise06_24.java
+++ src/chapters/Chapter6/Exercise06_24.java
@@ -11,29 +11,21 @@ public class Exercise06_24 {
     public static int getYear(){
         long totalMilliseconds = System.currentTimeMillis();
 
-        long calculatedMilliSec = 0;
+        long milliSecPerYear = (long) (3.1556926 * Math.pow(10,10));
         int count = 1970;
 
-        while ( calculatedMilliSec <= totalMilliseconds ) {
-
-            if(isLeapYear(count)){
-                calculatedMilliSec += 366 * 24 * 60 * 60 * 1000 ;
-            }
-            else {
-                calculatedMilliSec += 365 * 24 * 60 * 60 * 1000 ;
-            }
-            count++;
-
-        }
-        return count;
-
+        return (int)(totalMilliseconds / milliSecPerYear) + 1970 ;
 
 
     }
+
 
     public static boolean isLeapYear(int year){
+
         return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
+   // public static int month()
+
     //public static int day(int year){
 
     //}
---
+++ src/chapters/Chapter6/Exercise06_28.java
@@ -0,0 +1,18 @@
+package chapters.Chapter6;
+
+public class Exercise06_28 {
+    public static void main(String[] args) {
+
+        System.out.println("p\t\t\t\t2^p –1");
+        System.out.println("_______________________");
+
+        for (int p = 2 ; p <= 31 ; p ++ ){
+            System.out.println(p + "\t\t\t\t" + mersenne(p));
+        }
+    }
+
+    public static int mersenne(int num){
+
+        return (int) Math.pow(2,num) - 1 ;
+    }
+}
