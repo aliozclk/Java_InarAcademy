5ff4fe7033f68c7bc75e032657ad50b047145aa8
aliozclk <alliozcelik96@gmail.com>
2022-10-09 22:35:50 +0300

    new exercises added

---
+++ src/chapters/Chapter7/Exercise07_08.java
@@ -0,0 +1,43 @@
+package chapters.Chapter7;
+
+import java.util.Scanner;
+
+public class Exercise07_08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[10];
+
+        System.out.println("Enter ten double value: ");
+
+        readThem(numbers,input);
+        double avg = average(numbers);
+        System.out.println("The average is " + avg);
+
+    }
+    public static void readThem(double[] numbers,Scanner input) {
+        for (int i = 0; i < numbers.length ; i++) {
+            numbers[i]= input.nextDouble();
+
+        }
+    }
+    public static double average(double[] array){
+        double sum = 0 ;
+        for (int i = 0; i < array.length ; i++) {
+
+            sum += array[i];
+        }
+
+        return sum / array.length ;
+    }
+
+    public static int average(int[] array){
+        int sum = 0 ;
+        for (int i = 0; i < array.length ; i++) {
+
+            sum += array[i];
+        }
+
+        return sum / array.length ;
+    }
+
+}
---
+++ src/chapters/Chapter7/Exercise07_09.java
@@ -0,0 +1,7 @@
+package chapters.Chapter7;
+
+public class Exercise07_09 {
+    public static void main(String[] args) {
+
+    }
+}
