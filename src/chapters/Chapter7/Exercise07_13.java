7f723f4dbc502bcff429b194ef3262839aca8cdf
aliozclk <alliozcelik96@gmail.com>
2022-10-10 14:35:38 +0300

    new exercises added

---
+++ src/chapters/Chapter7/Exercise07_13.java
@@ -0,0 +1,33 @@
+package chapters.Chapter7;
+
+public class Exercise07_13 {
+    public static void main(String[] args) {
+        int[] numbers = new int[54];
+        for (int i = 0; i < numbers.length + 10; i++) {
+            int num = getRandom(numbers);
+            numbers[num - 1] = num;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ", ");
+            if (i % 10 == 0 && i != 0) {
+                System.out.println();
+            }
+        }
+
+    }
+
+    public static int getRandom(int... numbers) {
+        int rand = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            rand = (int) (Math.random() * 54) + 1;
+            if (numbers[rand - 1] == 0) {
+                return rand;
+
+            }
+
+        }
+
+        return rand;
+
+    }
+}
