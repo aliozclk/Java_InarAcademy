c49ccbe0fe3902023ad4ada895df6c6f407b4602
aliozclk <alliozcelik96@gmail.com>
2022-10-23 20:39:39 +0300

    new exercises added

---
+++ src/chapters/Chapter8/Exercise08_17.java
@@ -0,0 +1,57 @@
+package chapters.Chapter8;
+
+import java.util.Scanner;
+
+public class Exercise08_17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numOfBanks = input.nextInt();
+
+        double limit = input.nextDouble();
+        int count = 0 ;
+        double[][]  borrowers = new double[numOfBanks][numOfBanks];
+        boolean unsafe = false;
+
+        while (count < numOfBanks){
+            borrowers[count][count] = input.nextDouble();
+            int numOfLoans = input.nextInt();
+
+            for (int i = 0; i < numOfLoans; i++) {
+                int loanToBankId = input.nextInt();
+                borrowers[count][loanToBankId] = input.nextDouble();
+
+            }
+
+            int balance = 0 ;
+            for (int loanee = 0; loanee < numOfBanks; loanee++) {
+                balance += borrowers[count][loanee];
+            }
+            if(balance < limit){
+                for (int i = 0; i < numOfBanks; i++) {
+                    borrowers[i][count] = 0 ;
+                    unsafe = true;
+                }
+            }
+            count++;
+        }
+
+        if(unsafe){
+            System.out.print("Unsafe banks are ");
+            for (int row = 0; row < borrowers.length; row++) {
+                int balance = 0;
+                for (int col = 0; col < borrowers.length; col++) {
+                    balance += borrowers[row][col];
+                }
+                if(balance < limit){
+                    System.out.print(row + " ");
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
