4270c2785fc61f0f4a4b6aae4ce0aaec3ce862c9
aliozclk <alliozcelik96@gmail.com>
2022-10-23 21:26:11 +0300

    new exercises added

---
+++ src/chapters/Chapter8/Exercise08_18.java
@@ -0,0 +1,35 @@
+package chapters.Chapter8;
+
+public class Exercise08_18 {
+    public static void main(String[] args) {
+        int[][] m = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
+
+        shuffle(m);
+        printArray(m);
+
+
+    }
+
+    public static void shuffle(int[][] m) {
+        for (int row = 0; row < m.length; row++) {
+            int newRow = (int) (Math.random() * m.length);
+
+            for (int col = 0; col < m[row].length; col++) {
+                int newCol = (int) (Math.random() * m[row].length);
+                int temp = m[row][col];
+                m[row][col] = m[newRow][newCol];
+                m[newRow][newCol] = temp;
+            }
+        }
+    }
+
+    public static void printArray(int[][] chars) {
+        for (int row = 0; row < chars.length; row++) {
+            for (int col = 0; col < chars[row].length; col++) {
+                System.out.print(chars[row][col] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+}
