c984b5f697ae5e9ed74a06dcc1dd1bffcb530e29
aliozclk <alliozcelik96@gmail.com>
2022-11-02 19:49:57 +0300

    new exercises added

--- src/chapters/Chapter9/Exercise09_10/QuadraticEquation.java
+++ src/chapters/Chapter9/Exercise09_10/QuadraticEquation.java
@@ -1,13 +1,13 @@
 package chapters.Chapter9.Exercise09_10;
 
 public class QuadraticEquation {
-    private double a ;
-    private double b ;
-    private double c ;
+    private double a;
+    private double b;
+    private double c;
 
-    QuadraticEquation(double a ,double b , double c){
+    QuadraticEquation(double a, double b, double c) {
         this.a = a;
-        this.b = b ;
+        this.b = b;
         this.c = c;
     }
 
@@ -23,15 +23,22 @@ public class QuadraticEquation {
         return c;
     }
 
-    double getDiscriminant(){
-        return Math.pow(b , 2) - (4 * a * c );
+    double getDiscriminant() {
+        return Math.pow(b, 2) - (4 * a * c);
     }
 
-    double getRoot1(){
-        return (-b + Math.pow(getDiscriminant(), 0.5)) / 2 * a ;
+    double getRoot1() {
+        if (getDiscriminant() >= 0) {
+            return (-b + Math.pow(getDiscriminant(), 0.5)) / 2 * a;
+        }
+        return 0;
     }
 
-    double getRoot2(){
-        return (-b - Math.pow(getDiscriminant(), 0.5)) / 2 * a ;
+    double getRoot2() {
+        if (getDiscriminant() >= 0) {
+            return (-b - Math.pow(getDiscriminant(), 0.5)) / 2 * a;
+        }
+        return 0;
     }
+
 }
--- src/chapters/Chapter9/Exercise09_10/TestQuadraticEquation.java
+++ src/chapters/Chapter9/Exercise09_10/TestQuadraticEquation.java
@@ -5,5 +5,21 @@ import java.util.Scanner;
 public class TestQuadraticEquation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter values for a,b,c : ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        input.nextLine();
+
+        QuadraticEquation equation = new QuadraticEquation(a,b,c);
+        if(equation.getDiscriminant() > 0){
+            System.out.println("Root 1 : " + equation.getRoot1());
+            System.out.println("Root2 : " + equation.getRoot2());
+        } else if (equation.getDiscriminant() == 0) {
+            System.out.println("Root 1 : " + equation.getRoot1());
+        }else {
+            System.out.println("The equation has no roots.");
+        }
     }
 }
