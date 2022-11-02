2459a86930e9e8355a1621d029ae200575dc5e68
aliozclk <alliozcelik96@gmail.com>
2022-11-02 21:28:57 +0300

    new exercises added

---
+++ src/chapters/Chapter9/Exercise09_13/Location.java
@@ -0,0 +1,27 @@
+package chapters.Chapter9.Exercise09_13;
+
+public class Location {
+    public int row;
+    public int column;
+    public double maxValue;
+
+    Location(){
+
+    }
+
+    public static Location locateLargest(double[][] a){
+        Location location = new Location();
+        for (int row = 0; row < a.length; row++) {
+            for (int col = 0; col < a[row].length; col++) {
+                if (a[row][col] > location.maxValue){
+                    location.maxValue = a[row][col];
+                    location.row = row;
+                    location.column = col;
+                }
+
+            }
+
+        }
+        return location;
+    }
+}
---
+++ src/chapters/Chapter9/Exercise09_13/TestLocation.java
@@ -0,0 +1,34 @@
+package chapters.Chapter9.Exercise09_13;
+
+import java.util.Scanner;
+
+public class TestLocation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double[][] array = readThem(input);
+
+        Location x = Location.locateLargest(array);
+
+        System.out.println("Index :[" + x.row + "][" + x.column + "]" );
+        System.out.println("Max Value:" + x.maxValue);
+
+
+    }
+    public static double[][] readThem(Scanner input) {
+        System.out.println("Enter row :");
+        int r = input.nextInt();
+        System.out.println("Enter col :");
+        int c = input.nextInt();
+
+        double[][] numbers = new double[r][c];
+
+        for (int row = 0; row < numbers.length ; row++) {
+            for (int col = 0; col < numbers[row].length ; col++) {
+                numbers[row][col] = input.nextDouble();
+            }
+        }
+
+        return numbers;
+    }
+}
