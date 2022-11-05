373d3c046b4f357ca47d514bb7a401973ab353ef
aliozclk <alliozcelik96@gmail.com>
2022-11-05 19:14:39 +0300

    new exercises added

---
+++ src/weeks/week_13/Home.java
@@ -0,0 +1,136 @@
+package weeks.week_13;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Home {
+    private double yuzolcumu;
+    private int banyoSayisi;
+    private int odaSayisi;
+    private boolean garajVarMi;
+    private double fiyati;
+    private double kiraucreti;
+    private String cephesi;
+    private int yapimYili;
+
+
+    public Home() {
+        this(0, 0, 0);
+    }
+
+    public Home(double yuzolcumu, int banyoSayisi, int odaSayisi) {
+        this(yuzolcumu, banyoSayisi, odaSayisi, false, 0, 0, "", 1990);
+    }
+
+    public Home(double yuzolcumu, int banyoSayisi, int odaSayisi, boolean garajVarMi, double fiyati, double kiraucreti, String cephesi, int yapimYili) {
+        this.yuzolcumu = yuzolcumu;
+        this.banyoSayisi = banyoSayisi;
+        this.odaSayisi = odaSayisi;
+        this.garajVarMi = garajVarMi;
+        this.fiyati = fiyati;
+        this.kiraucreti = kiraucreti;
+        this.cephesi = cephesi;
+        this.yapimYili = yapimYili;
+
+    }
+
+    int yasHesapla(int yapimYili) {
+        return new GregorianCalendar().get(Calendar.YEAR) - yapimYili;
+    }
+
+    public double getYuzolcumu() {
+        return yuzolcumu;
+    }
+
+    public void setYuzolcumu(double yuzolcumu) {
+        this.yuzolcumu = yuzolcumu;
+    }
+
+    public int getBanyoSayisi() {
+        return banyoSayisi;
+    }
+
+    public void setBanyoSayisi(int banyoSayisi) {
+        this.banyoSayisi = banyoSayisi;
+    }
+
+    public int getOdaSayisi() {
+        return odaSayisi;
+    }
+
+    public void setOdaSayisi(int odaSayisi) {
+        this.odaSayisi = odaSayisi;
+    }
+
+    public boolean isGarajVarMi() {
+        return garajVarMi;
+    }
+
+    public void setGarajVarMi(boolean garajVarMi) {
+        this.garajVarMi = garajVarMi;
+    }
+
+    public double getFiyati() {
+        return fiyati;
+    }
+
+    public void setFiyati(double fiyati) {
+        this.fiyati = fiyati;
+    }
+
+
+    public double getKiraucreti() {
+        return kiraucreti;
+    }
+
+    public void setKiraucreti(double kiraucreti) {
+        this.kiraucreti = kiraucreti;
+    }
+
+    public String getCephesi() {
+        return cephesi;
+    }
+
+    public void setCephesi(String cephesi) {
+        this.cephesi = cephesi;
+    }
+
+    public int getYapimYili() {
+        return yapimYili;
+    }
+
+    public void setYapimYili(int yapimYili) {
+        this.yapimYili = yapimYili;
+    }
+
+    public int getYasi() {
+        return yasHesapla(getYapimYili());
+    }
+
+
+    public String homeString() {
+        return Double.toString(getYuzolcumu()) +"m^2 \n"+ Integer.toString(getBanyoSayisi()) + " adet banyo\n"
+                + Integer.toString(getOdaSayisi()) + " adet oda\n"
+                + ( isGarajVarMi() ? "garajı var " : "garajı yok") +"\n" + getCephesi() + " cephesi\n" + Double.toString(getFiyati()) + " TL\n"
+                + Double.toString(getKiraucreti()) + " kirası \n" + Integer.toString(getYasi()) + " yaşında ";
+    }
+
+    public void display(){
+        System.out.println("_____________________HOME DETAILS_________________________");
+        System.out.println( this.yuzolcumu  + "m^2 \n" +
+        this.banyoSayisi+  " adet banyo\n" +
+        this.odaSayisi +  " adet oda\n" +
+        (this.garajVarMi ? "garajı var \n" : "garajı yok\n") +
+        this.fiyati + " TL\n" +
+        this.kiraucreti +  "TL kira \n" +
+        this.cephesi + " cephesi\n"+
+        Integer.toString(getYasi()) + " yaşında " );
+    }
+
+    public void renovasyon(){
+        this.fiyati = this.fiyati * 1.1 ;
+        this.kiraucreti = this.kiraucreti * 1.2;
+    }
+
+
+}
---
+++ src/weeks/week_13/TestHome.java
@@ -0,0 +1,23 @@
+package weeks.week_13;
+
+public class TestHome {
+    public static void main(String[] args) {
+        Home home1 = new Home(150,2,4);
+        Home home2 = new Home();
+        Home home3 = new Home(180,2,6,true,1000000,20000,"kuzey",1453);
+
+
+        home3.display();
+        home3.renovasyon();
+        home3.display();
+
+        //Encapsulation-Kapsulleme
+
+
+        //Abstraction-Soyutlama
+
+        //Inheritance-Kalıtım
+
+
+    }
+}
