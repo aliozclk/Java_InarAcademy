f37c1dec3171361457f234cf9dfb2087f0e1d3f8
aliozclk <alliozcelik96@gmail.com>
2022-11-07 20:07:52 +0300

    new exercises added

--- src/chapters/Chapter10/Exercise10_05/Exercise10_5.java
+++ src/chapters/Chapter10/Exercise10_05/Exercise10_5.java
@@ -9,32 +9,31 @@ public class Exercise10_5 {
         //prompt and read number
         System.out.println("enter a positive integer");
         int num = input.nextInt();
+        StackOfIntegers reverse = new StackOfIntegers();
+        StackOfIntegers factors = new StackOfIntegers();
 
-        System.out.println("smallest factors in increasing order:");
-        StackOfIntegers factors = findSmallestFactors(num);
+        System.out.println("smallest factors in decreasing order:");
+        findSmallestFactors(num,factors);
+        decreasingOrder(factors, reverse);
         System.out.println();
 
 
         //display them in reverse order
         System.out.println("display them in reverse order:");
-        displayReverseOrder(factors);
+        displayReverseOrder(reverse);
     }
 
-    public static StackOfIntegers findSmallestFactors(int num) {
-        StackOfIntegers factors = new StackOfIntegers();
+    public static void findSmallestFactors(int num, StackOfIntegers factors) {
+
         for (int i = 2; i <= num; i++) {
             if (!isPrime(i)) {
                 continue;
             }
             while (num % i == 0) {
                 num /= i;
-                System.out.print(i + ", ");
                 factors.push(i);
             }
         }
-
-
-        return factors;
     }
 
     public static boolean isPrime(int number) {
@@ -46,11 +45,24 @@ public class Exercise10_5 {
         return true;
     }
 
+    public static void decreasingOrder(StackOfIntegers factors,StackOfIntegers rev){
+        int size = factors.getSize();
+        for (int i = 0; i < size; i++) {
+            reverseOrder(factors.peek(), rev);
+            System.out.print(factors.pop() + ", ");
+
+
+        }
+    }
+
+    public static void reverseOrder(int num,StackOfIntegers reverse){
+        reverse.push(num);
+    }
+
     public static void displayReverseOrder(StackOfIntegers factors){
         int size = factors.getSize();
         for (int i = 0; i < size; i++) {
-            System.out.print(factors.pop() + ",");
-
+            System.out.print(factors.pop() + ", ");
         }
     }
 
---
+++ src/chapters/Chapter10/Exercise10_06.java
@@ -0,0 +1,50 @@
+package chapters.Chapter10;
+
+import chapters.Chapter10.Exercise10_05.Exercise10_5;
+import chapters.Chapter10.Exercise10_05.StackOfIntegers;
+
+public class Exercise10_06 {
+    public static void main(String[] args) {
+        StackOfIntegers decreasingOrder = new StackOfIntegers();
+        StackOfIntegers increasingOrder = new StackOfIntegers();
+
+        for (int i = 2; i < 120 ; i++) {
+            if(Exercise10_5.isPrime(i)){
+                decreasingOrder.push(i);
+            }
+        }
+
+        System.out.println("Decreasing order :");
+        displayDecreasingOrder(decreasingOrder,increasingOrder);
+        System.out.println();
+        System.out.println("Increasing Order : ");
+        displayIncreasingOrder(increasingOrder);
+
+    }
+    public static void displayDecreasingOrder(StackOfIntegers factors,StackOfIntegers rev){
+        int size = factors.getSize();
+        for (int i = 0; i < size; i++) {
+            reverseOrder(factors.peek(), rev);
+            System.out.print(factors.pop() + ", ");
+            if((i + 1 ) % 5 == 0){
+                System.out.println();
+            }
+
+
+        }
+    }
+    public static void displayIncreasingOrder(StackOfIntegers factors){
+        int size = factors.getSize();
+        for (int i = 0; i < size; i++) {
+            System.out.print(factors.pop() + ", ");
+            if((i + 1 ) % 5 == 0){
+                System.out.println();
+            }
+        }
+    }
+
+    public static void reverseOrder(int num,StackOfIntegers reverse){
+        reverse.push(num);
+    }
+
+}
