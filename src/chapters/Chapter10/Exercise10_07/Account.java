581b6f7a8ea7ad56fa68df3f7225c1da12105a37
aliozclk <alliozcelik96@gmail.com>
2022-11-08 22:06:58 +0300

    new exercises added

---
+++ src/chapters/Chapter10/Exercise10_07/Account.java
@@ -0,0 +1,60 @@
+package chapters.Chapter10.Exercise10_07;
+
+import java.util.Date;
+
+public class Account {
+    private int id; // 0
+    private double balance ; //0
+    private double annualInterestRate; //0
+    private Date dateCreated;//date when account created
+
+    Account(){
+        dateCreated = new Date();
+    }
+    Account(int id , double balance ){
+        this.id = id;
+        this.balance = balance;
+        dateCreated = new Date();
+    }
+
+    int getId(){
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    double getBalance() {
+        return balance;
+    }
+
+    double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    Date getDateCreated() {
+        return dateCreated;
+    }
+
+    double getMonthlyInterest(){
+        return balance * (getAnnualInterestRate() / 12) ;
+    }
+
+    void withdraw(double amount){
+        balance -= amount ;
+    }
+
+    void deposit(double amount){
+        balance += amount;
+    }
+
+}
---
+++ src/chapters/Chapter10/Exercise10_07/Exercise10_07.java
@@ -0,0 +1,75 @@
+package chapters.Chapter10.Exercise10_07;
+
+import java.util.Scanner;
+
+public class Exercise10_07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Account[] accounts = new Account[10];
+
+        createAccounts(accounts);
+
+        while (true){
+            // prompt message
+            System.out.println("Enter an id");
+            int id = input.nextInt();
+            input.nextLine();
+
+            boolean didExitChose = false;
+            while (!didExitChose) {
+                //Main menu
+                System.out.println("Main menu\n" +
+                        "1: check balance\n" +
+                        "2: withdraw\n" +
+                        "3: deposit\n" +
+                        "4: exit\n" +
+                        "Enter a choice:");
+
+                //get command from user
+                int choice = input.nextInt();
+                input.nextLine();
+                System.out.println();
+
+                //execute this command
+                didExitChose = executeThisChoice(accounts, choice, id);
+            }
+
+        }
+    }
+
+    private static boolean executeThisChoice(Account[] accounts, int choice,int id) {
+        Scanner in = new Scanner(System.in);
+        switch (choice){
+            case 1 :
+                System.out.println("The balance is " + accounts[id].getBalance() );
+                System.out.println();
+                return false;
+            case 2 :
+                System.out.println("Enter an amount to withdraw:  " );
+                System.out.println();
+                int withdrawAmount = in.nextInt();
+                in.nextLine();
+                accounts[id].withdraw(withdrawAmount);
+                return false;
+            case 3 :
+                System.out.println("Enter an amount to deposit: " );
+                System.out.println();
+                int depositAmount = in.nextInt();
+                in.nextLine();
+                accounts[id].deposit(depositAmount);
+                return false;
+            case 4:
+                return true;
+        }
+
+        System.out.println("Invalid input. Please make a valid choice ...");
+        return false;
+    }
+
+    public static void createAccounts(Account[] accounts){
+        for (int i = 0; i < accounts.length; i++) {
+            accounts[i] = new Account((i+1) , 100);
+        }
+    }
+
+}
