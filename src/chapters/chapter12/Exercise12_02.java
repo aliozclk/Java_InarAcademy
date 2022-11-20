7c810a1e7fec7e947e8dcfea4a659245fb350d77
aliozclk <alliozcelik96@gmail.com>
2022-11-20 22:50:54 +0300

    new exercises added

---
+++ src/chapters/chapter12/Exercise12_02.java
@@ -0,0 +1,33 @@
+package chapters.chapter12;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise12_02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        boolean validInput = true;
+        int num1 = 0 ;
+        int num2 = 0 ;
+        while (validInput) {
+            System.out.println("Enter two integers : ");
+
+            try {
+
+                num1 = input.nextInt();
+
+                num2 = input.nextInt();
+
+                 validInput = false;
+            }catch (InputMismatchException ex){
+                System.out.println("Invalid Input ...");
+                input.nextLine();
+            }
+
+
+        }
+
+        System.out.println("Result : "+ ( num1 + num2) );
+    }
+}
