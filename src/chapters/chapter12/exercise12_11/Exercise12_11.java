f7c2f338ecc3855b102e6d734cc472aa9493310a
aliozclk <alliozcelik96@gmail.com>
2022-11-25 15:22:49 +0300

    new exercises added

--- src/chapters/chapter12/exercise12_11/Exercise12_11.java
+++ src/chapters/chapter12/exercise12_11/Exercise12_11.java
@@ -5,32 +5,47 @@ import java.util.Scanner;
 
 public class Exercise12_11 {
     public static void main(String[] args) throws FileNotFoundException, IllegalArgumentException {
-        if(args.length != 2 ){
-            System.out.println("Illegal argument input");
-            throw new IllegalArgumentException("Illegal argument");
+        if (args.length != 2) {
+           throw new IllegalArgumentException("Illegal Input...");
         }
 
-        String[] commandline = new String[2];
-        commandline = args[0].split(" ");
-        File file = new File(commandline[1]);
+
+        File file = new File(args[1]);
+        if (!file.exists()) {
+            throw new FileNotFoundException("File " + args[1] + " does not exist");
+        }
+
+
         String str = "";
-        String out = "";
+        System.out.println(file.canRead());
+        System.out.println(file.canWrite());
         try(
             PrintWriter output = new PrintWriter(file);
             Scanner scan = new Scanner(file);
         ) {
-            while (scan.hasNext()) {
-                System.out.println("File exist: " + file.exists());
+            while (scan.hasNextLine()) {
 
-                str = scan.nextLine();
-                if(str.equals(commandline[0])){
-                    str.replace(commandline[0],"");
-                    output.print(str);
+
+                str += scan.nextLine();
+                if(str.contains("John")){
+                    output.print(removedString(str,args[0]));
+                    System.out.println("delete");
                 }
-
-
             }
         }
+    }
 
+    public static String removedString(String str,String removerStr){
+        String removed = "";
+        String[] splitted = str.split(" ");
+
+        for (int i = 0; i < splitted.length; i++) {
+            if(splitted[i].equals(removerStr)){
+                splitted[i] = "";
+            }else {
+                removed += splitted[i];
+            }
+        }
+        return removed;
     }
 }
