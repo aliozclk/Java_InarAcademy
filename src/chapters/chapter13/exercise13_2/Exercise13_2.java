cf65f9a534c4fe7f9162a82d176afe506716c919
aliozclk <alliozcelik96@gmail.com>
2022-11-30 22:46:25 +0300

    new exercises added

---
+++ src/chapters/chapter13/exercise13_2/Exercise13_2.java
@@ -0,0 +1,23 @@
+package chapters.chapter13.exercise13_2;
+
+import java.util.ArrayList;
+
+public class Exercise13_2 {
+    public static void main(String[] args) {
+        ArrayList<Number> numbers = new ArrayList<>();
+
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(7.7);
+        numbers.add(9.9);
+        numbers.add(11.1113);
+        System.out.println(numbers.toString());
+        shuffle(numbers);
+        System.out.println(numbers.toString());
+
+    }
+    public static void shuffle(ArrayList<Number> list){
+        java.util.Collections.shuffle(list);
+
+    }
+}
