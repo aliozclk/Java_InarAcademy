71705972ae41e6267e4c9802bf1cfea1994a1d81
aliozclk <alliozcelik96@gmail.com>
2022-12-01 22:44:22 +0300

    new exercises added

--- src/chapters/chapter13/exercise13_09/Circle.java
+++ src/chapters/chapter13/exercise13_09/Circle.java
@@ -1,70 +1,66 @@
 package chapters.chapter13.exercise13_09;
 
-import chapters.chapter13.exercise13_10.GeometricObject;
-
 public class Circle extends GeometricObject implements Comparable<Circle> {
-    private double radius;
+  private double radius;
 
-    public Circle() {
+  public Circle() {
+  }
+
+  public Circle(double radius) {
+    this.radius = radius;
+  }
+
+  /** Return radius */
+  public double getRadius() {
+    return radius;
+  }
+
+  /** Set a new radius */
+  public void setRadius(double radius) {
+    this.radius = radius;
+  }
+
+  @Override /** Return area */
+  public double getArea() {
+    return radius * radius * Math.PI;
+  }
+
+  /** Return diameter */
+  public double getDiameter() {
+    return 2 * radius;
+  }
+
+  @Override /** Return perimeter */
+  public double getPerimeter() {
+    return 2 * radius * Math.PI;
+  }
+
+  /* Print the circle info */
+  public void printCircle() {
+    System.out.println("The circle is created " + getDateCreated() +
+      " and the radius is " + radius);
+  }
+
+  @Override
+  public String toString() {
+    return "Circle{" +
+            "radius=" + radius +
+            '}';
+  }
+
+  @Override
+  public int compareTo(Circle o) {
+    if(this.getArea() > o.getArea()){
+      return 1;
+    } else if (this.getArea() == o.getArea()) {
+      return 0;
+    }else {
+      return -1;
     }
+  }
 
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    /**
-     * Return radius
-     */
-    public double getRadius() {
-        return radius;
-    }
-
-    /**
-     * Set a new radius
-     */
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    @Override
-    /** Return area */ public double getArea() {
-        return radius * radius * Math.PI;
-    }
-
-    /**
-     * Return diameter
-     */
-    public double getDiameter() {
-        return 2 * radius;
-    }
-
-    @Override
-    /** Return perimeter */ public double getPerimeter() {
-        return 2 * radius * Math.PI;
-    }
-
-    @Override
-    public int compareTo(GeometricObject o) {
-        return 0;
-    }
-
-    @Override
-    public boolean equals(Circle o) {
-        return false;
-    }
-    
-
-    @Override
-    public int compareTo(Circle o) {
-        if (this.radius > o.radius) return 1;
-        else if (this.radius < o.radius) return -1;
-        else return 0;
-    }
-
-
-    /* Print the circle info */
-    public void printCircle() {
-        System.out.println("The circle is created " + getDateCreated() + " and the radius is " + radius);
-    }
-
+  @Override
+  public boolean equals(Object obj) {
+    return compareTo((Circle) obj) == 0;
+  }
 }
