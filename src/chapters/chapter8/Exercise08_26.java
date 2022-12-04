01d44cd70c5ef18fac409d9088fe17fbe3fac8f3
aliozclk <alliozcelik96@gmail.com>
2022-12-04 17:29:00 +0300

    new exercises added

--- src/chapters/chapter8/Exercise08_26.java
+++ src/chapters/chapter8/Exercise08_26.java
@@ -1,12 +1,19 @@
 package chapters.chapter8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise08_26 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double[][] matrix = new double[3][3];
+        //double[][] matrix = new double[3][3];
+        ArrayList<Double>[] matrix = new ArrayList[3];
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = new ArrayList<>();
+        }
+
         System.out.println("Enter a 3-by-3 matrix row by row: ");
         readThem(matrix, input);
 
@@ -16,40 +23,38 @@ public class Exercise08_26 {
 
     }
 
-    public static void readThem(double[][] numbers,Scanner input) {
-        for (int row = 0; row < numbers.length ; row++) {
-            for (int col = 0; col < numbers[row].length ; col++) {
-                numbers[row][col] = input.nextDouble();
+    public static void readThem(ArrayList<Double>[] numbers,Scanner input) {
+        for (int row = 0; row < 3 ; row++) {
+            for (int col = 0; col < 3 ; col++) {
+                numbers[row].add(input.nextDouble());
             }
         }
     }
 
-    public static double[][] sortRows(double[][] m){
+    public static void sortRows(ArrayList[] m){
         for (int rows = 0; rows < m.length; rows++) {
-            for (int i = 0; i < m.length; i++) {
+            for (int i = 0; i < m[rows].size(); i++) {
                 int minIndex = -1;
                 double min = Double.MAX_VALUE;
                 for (int j = i; j < m.length ; j++) {
 
-                    if(m[rows][j] < min){
-                        min = m[rows][j] ;
+                    if((double)m[rows].get(j) < min){
+                        min =(double) m[rows].get(j) ;
                         minIndex = j ;
                     }
                 }
                 double temp = 0;
-                temp = m[rows][i];
-                m[rows][i] = m[rows][minIndex];
-                m[rows][minIndex] = temp;
+                temp = (double) m[rows].get(i);
+                m[rows].set(i,m[rows].get(minIndex));
+                m[rows].set(minIndex,temp);
             }
         }
-
-        return m;
     }
 
-    public static void printArray(double[][] chars) {
+    public static void printArray(ArrayList[] chars) {
         for (int row = 0; row < chars.length; row++) {
-            for (int col = 0; col < chars[row].length; col++) {
-                System.out.print(chars[row][col] + " | ");
+            for (int col = 0; col < chars[row].size(); col++) {
+                System.out.print(chars[row].get(col)+ " | ");
             }
             System.out.println();
         }
