542f9ac81984a3988b1f327beb8ca20e1c51c8e6
aliozclk <alliozcelik96@gmail.com>
2022-12-05 23:11:56 +0300

    new exercises added

--- src/homework/arraylist/Exercise20_1.java
+++ src/homework/arraylist/Exercise20_1.java
@@ -15,21 +15,31 @@ public class Exercise20_1 {
             list.add(scan.nextLine());
         }
         scan.close();
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> sorted = new ArrayList<>();
 
         for (int i = 0; i < list.size(); i++) {
-            String[] words = list.get(i).split(" ");
-            for (int j = 0; j < words.length - 1; j++) {
-                for (int k = j+1; k < words.length; k++) {
-                    if(words[j].compareTo(words[k]) > 0 ){
-                        String temp = words[j];
-                        words[j] = words[k];
-                        words[k] = temp;
-                    }
+            String[] str = list.get(i).split("\\s");
+            for (int j = 0; j < str.length - 1; j++) {
+                words.add(str[j]);
+            }
+        }
+
+        for (int i = 0; i < words.size() - 1; i++) {
+            String minStr = words.get(i);
+            int minIndex = i;
+            for (int j = 0; j < words.size(); j++) {
+                if(minStr.compareToIgnoreCase(words.get(j)) > 0 ){
+                    minStr = words.get(j);
+                    minIndex = j;
                 }
             }
-            for (int j = 0; j < words.length; j++) {
-                System.out.print(words[j] + " ");
-            }
+            sorted.add(minStr);
+            words.remove(minIndex);
+        }
+
+        for (int i = 0; i < sorted.size(); i++) {
+            System.out.print(sorted.get(i) + " ");
         }
 
 
